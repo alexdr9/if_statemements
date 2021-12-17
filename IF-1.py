408a58f7b60989e6ac9d5155aa50b24f5d9df591
Alex.Drozd <a7ex92@hotmail.com>
2021-12-17 20:27:08 +0000

    init

--- IF-1.py
+++ IF-1.py
@@ -5,6 +5,7 @@ is_christmas = False                       # sets a false boolean for the variab
 if is_christmas:                           # initialises IF statement to print the below if True
     print("It's Christmaasssss!")
 else:                                      # if this is not true then prints the below string instead
-    print("It's not Christmas")
+    print("It's not Christmas x")
 
 # testing connection
+
