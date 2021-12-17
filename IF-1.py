d6c5478881dc43de1dfdaa338ac7275b3e6631ba
Alex.Drozd <a7ex92@hotmail.com>
2021-12-17 20:18:25 +0000

    init

---
+++ IF-1.py
@@ -0,0 +1,9 @@
+# -------------------------- day 5
+
+is_christmas = False                       # sets a false boolean for the variable container 'is_christmas'
+
+if is_christmas:                           # initialises IF statement to print the below if True
+    print("It's Christmaasssss!")
+else:                                      # if this is not true then prints the below string instead
+    print("It's not Christmas")
+
