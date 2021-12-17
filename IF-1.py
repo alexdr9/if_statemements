a347a719cda9976562a305271fae63be1b4491b7
Alex.Drozd <a7ex92@hotmail.com>
2021-12-17 21:09:59 +0000

    init

--- IF-1.py
+++ IF-1.py
@@ -2,10 +2,12 @@
 
 is_christmas = False                       # sets a false boolean for the variable container 'is_christmas'
 is_snowing = False
+is_wonderful_time = True
+is_december = True
 
-if is_christmas or is_snowing:                           # initialises IF statement to print the below if True
+if is_christmas or is_snowing:             # initialises IF statement to print the below if one or the other are True
     print("It's Christmaasssss! ..or it's snowing")
 else:                                      # if this is not true then prints the below string instead
     print("It's not Christmas and it's not snowing")
-
-    
+if is_december and is_wonderful_time:                         # prints string if both variables are True
+    print("We're simply having a wonderful Christmas time")
