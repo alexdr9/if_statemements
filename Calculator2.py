0aca85706d9aa7291b19357cccb0f320da2139b9
Alex.Drozd <a7ex92@hotmail.com>
2021-12-18 18:30:58 +0000

    init

--- Calculator2.py
+++ Calculator2.py
@@ -6,4 +6,11 @@ num2 = float(input("Enter the second number: "))
 
 if op == "+":
     print(num1 + num2)
-    
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid operator")
