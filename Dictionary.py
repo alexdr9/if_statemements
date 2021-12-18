c7a8259809461888a484aba33fdfae5b6f2cc95c
Alex.Drozd <a7ex92@hotmail.com>
2021-12-18 20:10:34 +0000

    init

--- Dictionary.py
+++ Dictionary.py
@@ -13,6 +13,11 @@ month_conversions = {                      # creates a dictionary called month_c
     "Oct": "October",
     "Nov": "November",
     "Dec": "December",
+    1: "First month"                     # numbers can also be used in dictionaries
 }                                        # } closes the dictionary
 
 print(month_conversions["Dec"])          # prints the value associated with the key
+print(month_conversions.get("Dec"))      # this .get function will also work here to get the value for Dec
+
+print(month_conversions.get("Luv", "Not a valid key"))      # if the program cannot find the key then "not a
+                                                            # valid key" will be printed
