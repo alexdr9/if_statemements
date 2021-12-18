f51237a43bf4a37d2d951d15f2b1a9f2735a91b1
Alex.Drozd <a7ex92@hotmail.com>
2021-12-18 18:24:14 +0000

    init

---
+++ Calculator2.py
@@ -0,0 +1,9 @@
+# ------------------ day 6
+
+num1 = float(input("Enter the first number: "))
+operator = input("Enter the operator: ")
+num2 = float(input("Enter the second number: "))
+
+if op == "+":
+    print(num1 + num2)
+    
