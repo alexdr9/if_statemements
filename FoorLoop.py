c0dcee2bce4a7daed623bedcb0a13105e7470ddd
Alex.Drozd <a7ex92@hotmail.com>
2021-12-18 22:29:29 +0000

    init

--- FoorLoop.py
+++ FoorLoop.py
@@ -12,5 +12,11 @@ print(len(instruments))                          # prints the number of items in
 for index in range(0, 10):                       # sets a variable (index) in a range between 0 and 10 (not including 10
     print(index)                                 # prints the specified index
 
-for index1 in range(len(instruments)):           # 
-    print(instruments[index1])                   # prints all items in instruments list
+for index1 in range(len(instruments)):           # creates a for loop and will print the length of the list
+    print(instruments[index1])                   # prints each individual item in the list
+
+for index2 in range(5):                          # creates a loop that repeats 5 times
+    if index2 == 0:                              # if this is the first loop then print the string
+        print("first iteration")
+    else:                                        # if the loop is on the second to fifth loop then print the string
+        print("not first")
