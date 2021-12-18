ba24d8dfb82a4f06c489c069004118f84d8a79d9
Alex.Drozd <a7ex92@hotmail.com>
2021-12-18 18:51:22 +0000

    init

--- Calculator2.py
+++ Calculator2.py
@@ -1,16 +1,17 @@
 # ------------------ day 6
 
-num1 = float(input("Enter the first number: "))
-operator = input("Enter the operator: ")
-num2 = float(input("Enter the second number: "))
+num1 = float(input("Enter the first number: "))     # num1 variable will be input by the user as a float (decimal number
+op = input("Enter the operator: ")                  # operator key entered by the user is stored in a variable container
+num2 = float(input("Enter the second number: "))    # num2 variable will be input by the user as a float (decimal number
 
-if op == "+":
-    print(num1 + num2)
-elif op == "-":
-    print(num1 - num2)
+if op == "+":                                       # if op input by user is equal to a plus symbol then
+    print(num1 + num2)                              # print num1 plus num2
+elif op == "-":                                     # if the input is not a plus symbol then
+    print(num1 - num2)                              # print num1 minus num2, and so on...
 elif op == "/":
     print(num1 / num2)
 elif op == "*":
     print(num1 * num2)
-else:
+else:                                               # if the input is none of the symbols above then print the below
     print("Invalid operator")
+
--- IfStatements2.py
+++ IfStatements2.py
@@ -1,14 +1,14 @@
 # ----------------------------- day 5
 
-def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 >= num3:
-        return num2
-    else:
+def max_num(num1, num2, num3):                            # defines a function to find the largest number out of the 3
+    if num1 >= num2 and num1 >= num3:                     # creates an if statement where if num1 is the largest
+        return num1                                       # to return num1 back to the user, if not then go to next line
+    elif num2 >= num1 and num2 >= num3:                   # else if statement to find if num2 is larger
+        return num2                                       # if true then returns num2 to the user
+    else:                              # else statement to close, if neither number is larger than num3 then print num3
         return num3
 
-print(max_num(422, 50, 63))
+print(max_num(422, 50, 63))            # print the larger number
 
 # !=     not equal to
 # ==     equal to
