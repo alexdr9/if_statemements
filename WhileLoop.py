a73e66bdd4b6b4a7d7a74fb5ad1e6140a3f48414
Alex.Drozd <a7ex92@hotmail.com>
2021-12-18 20:24:06 +0000

    init

--- WhileLoop.py
+++ WhileLoop.py
@@ -3,7 +3,7 @@
 # while loops will continue to repeat for as long as a certain condition is true
 
 i = 1                          # sets i as a variable and is equal to 1
-while i <= 10:                 # creates a while loop, as long as i is less then or equal to 10 then the loop continues
+while i <= 10:                 # creates a while loop, as long as i is less than or equal to 10 then the loop continues
     print(i)
     i += 1                     # (or,   i = i + 1)  adds 1 to variable per loop
 
