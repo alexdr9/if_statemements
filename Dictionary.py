d6104d1405e3364f735735734ff533d30ef83b60
Alex.Drozd <a7ex92@hotmail.com>
2021-12-18 19:10:56 +0000

    init

---
+++ Dictionary.py
@@ -0,0 +1,18 @@
+# ----------------- day 6
+
+month_conversions = {                      # creates a dictionary called month_conversions using a { symbol
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}                                        # } closes the dictionary
+
+print(month_conversions["Dec"])          # prints the full name of the month from the mentioned library
