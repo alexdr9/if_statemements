7df233fd95a5de55d6c9485c92256e7e995ab3b5
Alex.Drozd <a7ex92@hotmail.com>
2021-12-19 18:47:38 +0000

    init

--- 2DListsAndNestedLoops.py
+++ 2DListsAndNestedLoops.py
@@ -10,3 +10,11 @@ number_grid = [                  # creates a lists (array) called number_grid
 print(number_grid[0][0])         # prints row position 0, column position 0 (in this case being number 1 in the list)
 print(number_grid[1][2])         # prints number 6 from the lists (positions start from 0, not 1)
 
+for row in number_grid:          # creates a function for each row in the number grid
+    print(row)                   # to print the rows
+
+for row in number_grid:          # creates a function for each row in the number grid
+    for col in row:              # and for each column in the rows
+        print(col)               # prints each individual number
+
+
