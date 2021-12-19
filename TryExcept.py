0d8ed675ca64b9d9ad5ed54985c6fd67fb3a4193
Alex.Drozd <a7ex92@hotmail.com>
2021-12-19 21:10:13 +0000

    init

--- TryExcept.py
+++ TryExcept.py
@@ -3,7 +3,13 @@
 # Try / Except is a process of using code to find where an error is occurring in your code
 
 try:                                              # the code will first try the indented code
-    number = int(input("Enter a number: "))
+    value = 10 / 0                                # this will throw an error overruling the below error, because..
+                                                  # 10/0 is not possible in maths, so we need to make an exception
+    number = int(input("Enter a number: "))       # this will error if the user inputs a string instead of an int number
     print(number)
-except:                                           # if an error occurs then the below code will follow
+except ZeroDivisionError:                         # 10/0 will output s zerodiv error so we except it and print the below
+    print("Zero division error")
+except ValueError:                                # if user enters a string then python will output a value error
     print("Invalid input")                        # prints the string
+except ZeroDivisionError as err:                  # this will print the error in the below variable
+    print(err)
