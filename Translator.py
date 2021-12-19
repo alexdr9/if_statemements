d1b60d3c24093ffe7fe1171ce6cc8c633b4b1ef7
Alex.Drozd <a7ex92@hotmail.com>
2021-12-19 22:42:41 +0000

    init

--- Translator.py
+++ Translator.py
@@ -5,9 +5,9 @@ def translate(phrase):                             # defines a translation where
     for letter in phrase:                          # sets a loop, so for every letter in the entered phrase
         if letter.lower() in "aeiou":              # and if the letter in the phrase is a vowel (and change lower case)
             if letter.isupper():                   # if the letter entered is upper case
-                translation = translation + "Hurr" # then add the following string with a capital letter
-            else:                                  # if not..
-                translation = translation + "hurr" # the translation will add the string in lower case
+                translation = translation + "Hurr"        # then add the following string with a capital letter
+            else:                                         # if not..
+                translation = translation + "hurr"        # the translation will add the string in lower case
         else:                                      # if this above is not true and a consonant is entered instead...
             translation = translation + letter     # then the program will add the letter to the output
     return translation                             # returns the variable
