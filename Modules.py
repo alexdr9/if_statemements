789b534756a5a1a2b18cef5f386119753c876936
Alex.Drozd <a7ex92@hotmail.com>
2021-12-20 23:31:11 +0000

    init

---
+++ Modules.py
@@ -0,0 +1,8 @@
+# ------------------- day 8
+
+
+# modules are python files that we can import into a new or existing project. The modules contain functions, variables
+# and other stored data that can be used in a project after being imported.
+
+import input                        # imports all variables, functions, etc. from the input.py file 
+
