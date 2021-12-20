dafff662d06ef64fb5495f8cd0d7e00aeccc62d6
Alex.Drozd <a7ex92@hotmail.com>
2021-12-20 23:35:34 +0000

    init

--- Modules.py
+++ Modules.py
@@ -4,5 +4,6 @@
 # modules are python files that we can import into a new or existing project. The modules contain functions, variables
 # and other stored data that can be used in a project after being imported.
 
-import input                        # imports all variables, functions, etc. from the input.py file 
+import input                        # imports all variables, functions, etc. from the input.py file
 
+# PIP is used to install python modules from other sources 
