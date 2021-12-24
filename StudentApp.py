c2b781613e8f45512845b8614acd07e3ff7b02c9
Alex.Drozd <a7ex92@hotmail.com>
2021-12-24 02:40:02 +0000

    init

---
+++ StudentApp.py
@@ -0,0 +1,9 @@
+# -------- day 11
+
+from Student import Student
+
+student1 = Student("Oscar", "Accounting", 2.1)
+student2 = Student("Daeny", "Rocket Scientist", 2.1)
+student3 = Student("Bob", "Clown", 1.9)
+
+print(student1.on_honour_roll())
